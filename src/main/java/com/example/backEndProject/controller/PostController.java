82bb0026f0ac9b0c584083e77f2ac54ab7b9927f
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-20 19:57:15 +0100

    Controllers and get_all mapping added

--- src/main/java/com/example/backEndProject/controller/PostController.java
+++ src/main/java/com/example/backEndProject/controller/PostController.java
@@ -1,4 +1,23 @@
 package com.example.backEndProject.controller;
 
+import com.example.backEndProject.model.Post;
+import com.example.backEndProject.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+//    Mapping Methods
+
+    @GetMapping("/list_all_posts")
+    public List<Post> getAll() {
+        return postService.getAll();
+    }
 }
--- src/main/java/com/example/backEndProject/controller/UserController.java
+++ src/main/java/com/example/backEndProject/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.example.backEndProject.controller;
 
+import com.example.backEndProject.model.User;
+import com.example.backEndProject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+//    Mapping Methods
+
+    @GetMapping("/list_all_users")
+    public List<User> getAll() {
+        return userService.getAll();
+    }
+
 }
