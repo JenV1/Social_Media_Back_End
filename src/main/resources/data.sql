5f1ab00be9293be2a1e1f563aacf51141cb53f15
scottiee-jc <s.christie496@gmail.com>
2022-05-21 21:09:05 +0100

    ADDED gordon ramsey post

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
+--users here
+
 INSERT INTO users (company,date_of_birth,name,password)
 VALUES ('Bright','Wednesday','jem','password');
-
-INSERT INTO posts (content_text,number_of_likes) VALUES ('hellooo',200)
 
 INSERT INTO users (company,date_of_birth,name,password)
 VALUES ('BNTA', '13/09/1998', 'Scott', 'chickenfella');
@@ -14,3 +14,10 @@ VALUES ('BNTA', '23/01/2004', 'Mohamed', 'CLIlover');
 
 INSERT INTO users (company,date_of_birth,name,password)
 VALUES ('GOD', '25/12/2000BC', 'Jesus', 'bigboi');
+
+
+-- posts here
+
+INSERT INTO posts (content_text,number_of_likes) VALUES ('hellooo',200)
+INSERT INTO posts (content_text,number_of_likes) VALUES ('wow. the most aMAzing fillet mignon, lathered in THE most incredible peppercorn sauce. THAT is food!',1000)
+INSERT INTO posts (content_text,number_of_likes) VALUES ('wow. the most aMAzing fillet mignon, lathered in THE most incredible peppercorn sauce. THAT is food!',1000)
