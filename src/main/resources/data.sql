cb1f31d5767bbb17349269fb5304c9b4a7c79851
scottiee-jc <s.christie496@gmail.com>
2022-05-21 15:57:35 +0100

    second db commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,10 @@ INSERT INTO users (company,date_of_birth,interests,name,password) VALUES ('Brigh
 ,com.example.backEndProject.model.Interests.AI,'Jem','qqqqq');
 
 INSERT INTO users (company,date_of_birth,interests,name,password) VALUES ('BNTA', '13/09/1998',
-com.example.backEndProject.model.Interests.AI, 'Scott', 'chickenfella')
+com.example.backEndProject.model.Interests.AI, 'Scott', 'chickenfella');
+
+INSERT INTO users (company,date_of_birth,interests,name,password) VALUES ('BNTA', '11/07/1864',
+com.example.backEndProject.model.Interests.AI, 'Lewis', 'koolkode');
+
+INSERT INTO users (company,date_of_birth,interests,name,password) VALUES ('BNTA', '23/01/2004',
+com.example.backEndProject.model.Interests.AI, 'Mohamed', 'CLIlover')
