02497173e36aaa6ca8f66a8118ad36c669b705fc
scottiee-jc <s.christie496@gmail.com>
2022-05-21 15:29:05 +0100

    first db commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (company,date_of_birth,interests,name,password) VALUES ('Bright','today'
 ,com.example.backEndProject.model.Interests.AI,'Jem','qqqqq');
+
+INSERT INTO users (company,date_of_birth,interests,name,password) VALUES ('BNTA', '13/09/1998',
+com.example.backEndProject.model.Interests.AI, 'Scott', 'chickenfella')
