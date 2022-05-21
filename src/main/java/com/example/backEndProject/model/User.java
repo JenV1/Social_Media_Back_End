58e6f1aed1bec1ae43e0352893549e4cbd13f7d3
Cem Gurhan <cemg@live.co.uk>
2022-05-21 12:02:58 +0100

    Added @Table annotations to User and Post classes

--- src/main/java/com/example/backEndProject/model/User.java
+++ src/main/java/com/example/backEndProject/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.ArrayList;
 
 @Entity
+@Table(name = "users")
 public class User {
 
 //    Attributes
--- src/main/java/com/example/backEndProject/repository/UserRepository.java
+++ src/main/java/com/example/backEndProject/repository/UserRepository.java
@@ -2,6 +2,10 @@ package com.example.backEndProject.repository;
 
 import com.example.backEndProject.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
 }
