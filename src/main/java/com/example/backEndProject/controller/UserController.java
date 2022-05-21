59da25f5ca18bf159e4b5db78bd3228f548872dd
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-05-21 15:48:10 +0100

    postmap for user

--- src/main/java/com/example/backEndProject/controller/UserController.java
+++ src/main/java/com/example/backEndProject/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.backEndProject.model.User;
 import com.example.backEndProject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,7 +38,11 @@ public class UserController {
         return userService.findById(id);
     }
 
-    @PostMapping
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user){
+        User savedUser = userService.save(user);
+    }
+
 
 
 
