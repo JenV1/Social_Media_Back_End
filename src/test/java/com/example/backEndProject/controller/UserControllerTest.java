d72547faea7899dd35cabab20793e53a950b793f
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-21 13:53:08 +0100

    Added Test files

---
+++ src/test/java/com/example/backEndProject/controller/UserControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.backEndProject.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void listAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
