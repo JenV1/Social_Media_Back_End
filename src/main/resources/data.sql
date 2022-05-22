1c674974d9d013d2a38b0a4fda675024b4ceeb23
scottiee-jc <s.christie496@gmail.com>
2022-05-22 12:02:58 +0100

    inspirational dog story

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,7 @@ VALUES ('BNTA', 'Software Engineer','11/07/1864', 'Lewis', 'koolkode');
 INSERT INTO users (company,role,date_of_birth,name,password)
 VALUES ('BNTA', 'Software Engineer','23/01/2004', 'Mohamed', 'CLIlover');
 INSERT INTO users (company,role,date_of_birth,name,password)
-VALUES ('GOD', 'Son of God','25/12/2000BC', 'Jesus', 'holy');
+VALUES ('GOD', 'Son of God','25/12/6BC', 'Jesus', 'holy');
 INSERT INTO users (company,role,date_of_birth,name,password)
 VALUES ('Hells Kitchen','Head Chef','08/11/1966', 'Gordon Ramsey', 'lambsauce');
 INSERT INTO users (company,role,date_of_birth,name,password)
@@ -25,10 +25,16 @@ VALUES ('Tegrity','Hemp Farmer','05/04/1977', 'Randy Marsh', 'theytookourjobs');
 
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (1, 'Delighted to be partaking in the BNTA back-end API project. Excited to work with some of the biggest names in the business on this!',18);
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (2, 'Filling out a database can be monotonous. Thats why its fun to be creative! Adding users with a personality makes it worthwhile.', 1);
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, 'Yesterday i missed a crucial interview after stopping to aid a dying dog. The next day i got a call from the interviewer asking me to come in for a second chance. When i arrived, the interviewer was waiting for me. It was the dog.',1000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, 'The following thread is a true story; an experience that has, and will continue to change my life forever...', 80)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, '(1) Yesterday, i was heading for an interview, when i came across a starving dog. I knew, in that moment, it was more important to help the dog...', 56)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, '(2) ... So I fed the dog, bathed and clothed it. Found a rescue centre nearby. Of course, I missed the interview, but I was confident I did the right thing, despite missing out on my dream job.', 34);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, '(3) ... But the next day, a funny thing happened. The office called me and I told them what happened. They asked me if I would like to come down to the office that day, so I did... ', 47)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, '(4) ... I was called into the boardroom after waiting nervously for some time. Then I saw the interviewer. It was the dog.', 700)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (5, 'Thanks for all the birthday messages! Havent been on this account in months!',
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (6, 'Wow. The most aMAzing fillet mignon, lathered in THE most incredible peppercorn sauce. THAT is food!',1000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (6, 'Wow. Seriously? Wow. No come on. The post is RAWWWW!', 1000)
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, '𝐍𝐨𝐭𝐫𝐞 𝐡𝐢𝐬𝐭𝐨𝐢𝐫𝐞 𝐬’𝐞́𝐜𝐫𝐢𝐭 𝐢𝐜𝐢. 𝐈𝐜𝐢 𝐜’𝐞𝐬𝐭 𝐏𝐚𝐫𝐢𝐬 (et l argent hehe!)', 200000);
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, '𝐍𝐨𝐭𝐫𝐞 𝐡𝐢𝐬𝐭𝐨𝐢𝐫𝐞 𝐬’𝐞́𝐜𝐫𝐢𝐭 𝐢𝐜𝐢. 𝐈𝐜𝐢 𝐜’𝐞𝐬𝐭 𝐏𝐚𝐫𝐢𝐬 (et l argent hehe!)', 200000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, 'Hope you enjoyed my hat-trick last night!!!', 1000000);
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, 'I am making a series of 4 reasons on why software engineering is THE industry to break into right now, and for the future!', 50);
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, '(1) The industry is growing... big time! With the U.S. Software Engineering market expected to grow to $44.54bn by 2025 (CAGR 7.2%), there should be plenty of upcoming opportunities for keen coders!', 70);
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, '(2) Autonomy! You have the freedom and opportunity to innovate and create using new technologies in lots of different projects. But remember - With great power, comes great responsibility... Dont create an Ultron!', 87);
