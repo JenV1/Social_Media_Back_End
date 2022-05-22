88ea2a6a0354151809db37873f1f674c293eaea0
Cem Gurhan <cemg@live.co.uk>
2022-05-23 00:40:53 +0100

    created a user login and heart comment put mapping method. Yet to be tested.

--- src/main/java/com/example/backEndProject/controller/CommentController.java
+++ src/main/java/com/example/backEndProject/controller/CommentController.java
@@ -1,9 +1,38 @@
 package com.example.backEndProject.controller;
 
+import com.example.backEndProject.EntityManager.EMF;
+import com.example.backEndProject.model.Comment;
+import com.example.backEndProject.model.User;
+import com.example.backEndProject.service.CommentService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
 
 @RestController
 public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService){
+        this.commentService = commentService;
+    }
+
+    @PutMapping("heartComment")
+    public String heartComment(@RequestParam int user_id,
+                               @RequestParam String user_name,
+                               @RequestParam String password,
+                               @RequestParam int postId
+                               ){
+
+        return commentService.heartComment(user_id,user_name,password,postId);
+
+
+
+
+    }
 
 
 }
--- src/main/java/com/example/backEndProject/service/CommentService.java
+++ src/main/java/com/example/backEndProject/service/CommentService.java
@@ -1,8 +1,43 @@
 package com.example.backEndProject.service;
 
+import com.example.backEndProject.EntityManager.EMF;
+import com.example.backEndProject.model.Comment;
+import com.example.backEndProject.model.Post;
+import com.example.backEndProject.model.User;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 @Service
 public class CommentService {
+
+    public String heartComment(int user_id, String user_name, String password, int postId ){
+
+
+        EntityManager em =  EMF.get().createEntityManager();
+        User user = em.createQuery("SELECT user FROM users WHERE id = ?1",User.class)
+                .setParameter(1,user_id)
+                .getSingleResult();
+
+
+        Query getPostUserIdQuery = em.createNativeQuery("SELECT user_id FROM posts WHERE id = ?1");
+        getPostUserIdQuery.setParameter(1,postId);
+        int postUserId = ( (Number) getPostUserIdQuery.getSingleResult()).intValue();
+
+
+
+        if(user.getName().equals(user_name) && user.getPassword().equals(password) && postUserId == user_id){
+
+            return "Access Granted, comment hearted";
+
+
+        }
+
+        return "Access denied";
+
+    }
+
+
 }
