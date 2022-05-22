37e1445889e0ee2a0e1c6314747f6e5a432c7dd8
scottiee-jc <s.christie496@gmail.com>
2022-05-22 10:03:41 +0100

    added new users in db

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,28 +1,29 @@
 --users here
 
 INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('Bright','Wednesday','Jem','password');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('BNTA', '13/09/1998', 'Scott', 'chickenfella');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('BNTA', '11/07/1864', 'Lewis', 'koolkode');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('BNTA', '23/01/2004', 'Mohamed', 'CLIlover');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('GOD', '25/12/2000BC', 'Jesus', 'holy');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('Hells Kitchen', '25/12/2000BC', 'Gordon Ramsey', 'lambsauce');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('PSG', '25/12/2000BC', 'Kylian Mbappe', 'lambsauce');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('Hells Kitchen', '25/12/2000BC', 'Gordon Ramsey', 'lambsauce');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('Hells Kitchen', '25/12/2000BC', 'Gordon Ramsey', 'lambsauce');
-INSERT INTO users (company,date_of_birth,name,password)
-VALUES ('Hells Kitchen', '25/12/2000BC', 'Gordon Ramsey', 'lambsauce');
+VALUES ('BNTA','Software Engineer','Wednesday','Jem','password');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('BNTA','Software Engineer','13/09/1998', 'Scott', 'chickenfella');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('BNTA', 'Software Engineer','11/07/1864', 'Lewis', 'koolkode');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('BNTA', 'Software Engineer','23/01/2004', 'Mohamed', 'CLIlover');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('GOD', 'Son of God','25/12/2000BC', 'Jesus', 'holy');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('Hells Kitchen','Head Chef','08/11/1966', 'Gordon Ramsey', 'lambsauce');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('PSG','Sporting Director/Owner/Football Player','10/12/1998', 'Kylian Mbappe', 'money');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('Bloggs Inc.','Blogger','tomorrow','Joe Bloggs','blogg');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('Liverpool FC','Football player','01/01/156AD', 'James Milner', 'ribena');
+INSERT INTO users (company,role,date_of_birth,name,password)
+VALUES ('Tegrity','Hemp Farmer','05/04/1977', 'Randy Marsh', 'theytookourjobs');
 
 -- posts here
 
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (1, 'delighted to be partaking in the BNTA back-end API project. Excited to work with some of the biggest names in the business on this!',200,000,000)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (1, 'delighted to be partaking in the BNTA back-end API project. Excited to work with some of the biggest names in the business on this!',200000000)
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (6, 'wow. the most aMAzing fillet mignon, lathered in THE most incredible peppercorn sauce. THAT is food!',1000)
 INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, 'yesterday i missed a crucial interview after stopping to aid a dying dog. the next day i got a call from the interviewer asking me to come in for a second chance. when i arrived, the interviewer was waiting for me. it was the dog.',1000)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, '𝐍𝐨𝐭𝐫𝐞 𝐡𝐢𝐬𝐭𝐨𝐢𝐫𝐞 𝐬’𝐞́𝐜𝐫𝐢𝐭 𝐢𝐜𝐢. 𝐈𝐜𝐢 𝐜’𝐞𝐬𝐭 𝐏𝐚𝐫𝐢𝐬 (et l argent hehe!)', 200000)
