bf96ba9f09fdaa91319a65c9b327ea84d8c7f53e
scottiee-jc <s.christie496@gmail.com>
2022-05-22 11:29:46 +0100

    some more db changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 --users here
 
-INSERT INTO users (company,date_of_birth,name,password)
+INSERT INTO users (company,role,date_of_birth,name,password)
 VALUES ('BNTA','Software Engineer','Wednesday','Jem','password');
 INSERT INTO users (company,role,date_of_birth,name,password)
 VALUES ('BNTA','Software Engineer','13/09/1998', 'Scott', 'chickenfella');
@@ -23,7 +23,16 @@ VALUES ('Tegrity','Hemp Farmer','05/04/1977', 'Randy Marsh', 'theytookourjobs');
 
 -- posts here
 
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (1, 'delighted to be partaking in the BNTA back-end API project. Excited to work with some of the biggest names in the business on this!',200000000)
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (6, 'wow. the most aMAzing fillet mignon, lathered in THE most incredible peppercorn sauce. THAT is food!',1000)
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, 'yesterday i missed a crucial interview after stopping to aid a dying dog. the next day i got a call from the interviewer asking me to come in for a second chance. when i arrived, the interviewer was waiting for me. it was the dog.',1000)
-INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, '𝐍𝐨𝐭𝐫𝐞 𝐡𝐢𝐬𝐭𝐨𝐢𝐫𝐞 𝐬’𝐞́𝐜𝐫𝐢𝐭 𝐢𝐜𝐢. 𝐈𝐜𝐢 𝐜’𝐞𝐬𝐭 𝐏𝐚𝐫𝐢𝐬 (et l argent hehe!)', 200000)
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (1, 'Delighted to be partaking in the BNTA back-end API project. Excited to work with some of the biggest names in the business on this!',18);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (2, 'Filling out a database can be monotonous. Thats why its fun to be creative! Adding users with a personality makes it worthwhile.', 1);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (3, 'Yesterday i missed a crucial interview after stopping to aid a dying dog. The next day i got a call from the interviewer asking me to come in for a second chance. When i arrived, the interviewer was waiting for me. It was the dog.',1000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (6, 'Wow. The most aMAzing fillet mignon, lathered in THE most incredible peppercorn sauce. THAT is food!',1000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, '𝐍𝐨𝐭𝐫𝐞 𝐡𝐢𝐬𝐭𝐨𝐢𝐫𝐞 𝐬’𝐞́𝐜𝐫𝐢𝐭 𝐢𝐜𝐢. 𝐈𝐜𝐢 𝐜’𝐞𝐬𝐭 𝐏𝐚𝐫𝐢𝐬 (et l argent hehe!)', 200000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (7, '𝐍𝐨𝐭𝐫𝐞 𝐡𝐢𝐬𝐭𝐨𝐢𝐫𝐞 𝐬’𝐞́𝐜𝐫𝐢𝐭 𝐢𝐜𝐢. 𝐈𝐜𝐢 𝐜’𝐞𝐬𝐭 𝐏𝐚𝐫𝐢𝐬 (et l argent hehe!)', 200000);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, 'I am making a series of 4 reasons on why software engineering is THE industry to break into right now, and for the future!', 50);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, '(1) The industry is growing... big time! With the U.S. Software Engineering market expected to grow to $44.54bn by 2025 (CAGR 7.2%), there should be plenty of upcoming opportunities for keen coders!', 70);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, '(2) Autonomy! You have the freedom and opportunity to innovate and create using new technologies in lots of different projects. But remember - With great power, comes great responsibility... Dont create an Ultron!', 87);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, '(3) Professional Development! There are plenty of opportunities to learn new programming languages in a continually changing, dynamic industry environment. Whats not to like?', 100);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, '(4) Collaboration! Having the opportunity to work with an interesting and diverse set of individuals, all with different intellectual curiosities for the subject, makes SE a fantastic choice to build a career!', 101);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (8, 'Hope you all enjoyed this thread! Dont forget to absolutely obliterate that like button and subscribe!', 89);
+INSERT INTO posts (user_id, content_text,number_of_likes) VALUES (9, 'Just seen a new ribena has been released. Ecstatic.', 100000);
