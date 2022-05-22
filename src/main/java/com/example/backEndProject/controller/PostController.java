b3dfadec4bbf151409d6a2528f5708815a019fbf
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-22 15:55:26 +0100

    Combined the service and controller classes properly

--- src/main/java/com/example/backEndProject/controller/PostController.java
+++ src/main/java/com/example/backEndProject/controller/PostController.java
@@ -20,49 +20,33 @@ import java.util.NoSuchElementException;
 @RestController
 public class PostController {
 
-    private PostRepository postRepository;
+
     private PostService postService;
 
-    public PostController(PostRepository postRepository){
-        this.postRepository = postRepository;
+    public PostController(PostService postService){
+        this.postService = postService;
     }
 
 //    Get Mapping Methods
 
     @GetMapping("/list_all_posts")
     public List<Post> getAll() {
-        return postRepository.findAll();
+        return postService.getAll();
     }
 
     @GetMapping("/post/{id}")
     public Post findPostByID(@PathVariable("id") Long id){
-        return findPostByID(id);
+        return postService.findPostByID(id);
     }
 
     @GetMapping("/numberOfLikesOnPost/{id}")
     public int findLikesByID(@PathVariable("id") Long id) {
-
-//        Inspired by the way likes are shown on posts on social media applications
-
-        Post current = null;
-        try {
-            current = postRepository.findById(id).get();
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            System.out.println("No matching post could be found for id: " + id);
-        }
-
-        return current.getNumber_of_likes();
+        return postService.findLikesByID(id);
     }
 
     @GetMapping("/searchForKeyword/{keyword}")
     public List searchPostsForKeyword(@PathVariable("keyword") String keyword) {
-
-        return postRepository.findAll().stream()
-                .map(Post::getContent_text)
-                .filter(s -> s.contains(keyword))
-                .toList();
-
+        return postService.searchPostsForKeyword(keyword);
     }
 
 
@@ -71,63 +55,13 @@ public class PostController {
     @PutMapping("/addLikeToPost/{id}")
     public Post updateLikeCount(@PathVariable("id") Long id) throws NoSuchElementException {
 
-//        Adds like to specific post by id.
-//        Try catch statement for the scenario where id is not found.
-//        Created current outide of try catch to ensure it was within scope for the return statement.
-
-        Post current = null;
-        try {
-            current = postRepository.findById(id).get();
-            current.setNumber_of_likes(current.getNumber_of_likes() + 1);
-            postRepository.save(current);
-
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            System.out.println("No matching post could be found for id: " + id);
-        }
-
-        return current;
+        return postService.updateLikeCount(id);
     }
 
     @PutMapping("/editOldPost/{id}")
     public Post editPost(@PathVariable("id") Long id,
                          @RequestBody String new_content) throws NoSuchElementException, IOException {
-
-//        Edits already established post by id.
-//        Try catch statement for the scenario where id is not found.
-//        Created current outside of try catch to ensure it was within scope for the return statement.
-
-        Post current = null;
-        try {
-            current = postRepository.findById(id).get();
-            current.setContent_text(new_content);
-            postRepository.save(current);
-
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            System.out.println("No matching post could be found for id: " + id);
-        }
-
-        File myFile = new File("src/all_posts_and_post_edits.txt");
-        if (!myFile.exists()) {
-            try {
-                myFile.createNewFile();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-
-        FileWriter fileWriter = new FileWriter(myFile, true);
-        PrintWriter printWriter = new PrintWriter(fileWriter);
-        printWriter.println(current.getContent_text());
-        printWriter.println(LocalDateTime.now());
-        printWriter.println(current.getUser().getName());
-        printWriter.println("");
-
-        printWriter.close();
-
-        return current;
+        return postService.editPost(id, new_content);
     }
 
 
--- src/main/java/com/example/backEndProject/controller/UserController.java
+++ src/main/java/com/example/backEndProject/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.backEndProject.controller;
 import com.example.backEndProject.model.Post;
 import com.example.backEndProject.model.User;
 import com.example.backEndProject.repository.UserRepository;
+import com.example.backEndProject.service.UserService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,151 +13,75 @@ import java.util.stream.Collectors;
 @RestController
 public class UserController {
 
-
-    private UserRepository userRepository;
-
-//    private UserService userService;
-
-    public UserController(UserRepository userRepository){
-        this.userRepository = userRepository;
-    }
-//    public UserController(UserService userService) {this.userService = userService;}
+    private UserService userService;
+    public UserController(UserService userService) {this.userService = userService;}
 
 //    Mapping Methods
 
     @GetMapping("/list_all_users")
     public List<User> getAll() {
-        return userRepository.findAll();
+        return userService.getAll();
     }
 
     @GetMapping("/users_postgres")
     public String listAll(Model model) {
-        List<User> listUsers = userRepository.findAll();
+        List<User> listUsers = userService.getAll();
         model.addAttribute("listUsers", listUsers);
 
         return "students";
     }
 
     @GetMapping("/users/{id}")
-    public Optional<User> findById(@PathVariable Long id){
-        return userRepository.findById(id);
+    public User findById(@PathVariable Long id){
+        return userService.findById(id);
     }
 
     @PostMapping("/users")
     public void createUser(@RequestBody User user){
-        User savedUser = userRepository.save(user);
+        User savedUser = userService.save(user);
     }
 
     @GetMapping("/searchForUserByName/{keyword}")
-    public Set<String> searchUsersForKeyword(@PathVariable("keyword") String keyword) {
-
-//        Gets all users and converts to a stream
-//        Extracts the name from each user and maps them
-//        Filters name dependent on whether they contain the specified keyword
-//        Returns list of posts
-
-        Set<String> foundUsers = userRepository.findAll().stream()
-                .map(User::getName)
-                .filter(s -> s.contains(keyword))
-                .collect(Collectors.toSet());
-
-        if (foundUsers.isEmpty()) {
-            foundUsers.add("No users found :(");
-        }
-
-        return foundUsers;
-
-
+    public List<String> searchUsersForKeyword(@PathVariable("keyword") String keyword) {
+        return userService.searchUsersForKeyword(keyword);
     }
+
 
 ///    Put Methods
 
+
     @PutMapping("/editName/{id}")
     public User editName(@PathVariable("id") Long id,
-                         @RequestBody String new_name) throws NoSuchElementException {
+                         @RequestBody String new_name){
 
-//        Edits already established post by id.
-//        Try catch statement for the scenario where id is not found.
-//        Created current outside of try catch to ensure it was within scope for the return statement.
-
-        User current = null;
-        try {
-            current = userRepository.findById(id).get();
-            current.setName(new_name);
-            userRepository.save(current);
-
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            System.out.println("No matching post could be found for id: " + id);
-        }
-
-        return current;
+        return userService.editName(id, new_name);
     }
 
     @PutMapping("/editCompany/{id}")
     public User editCompany(@PathVariable("id") Long id,
-                         @RequestBody String new_company) throws NoSuchElementException {
+                         @RequestBody String new_company) {
 
-//        Edits already established post by id.
-//        Try catch statement for the scenario where id is not found.
-//        Created current outside of try catch to ensure it was within scope for the return statement.
-
-        User current = null;
-        try {
-            current = userRepository.findById(id).get();
-            current.setCompany(new_company);
-            userRepository.save(current);
-
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            System.out.println("No matching post could be found for id: " + id);
-        }
-
-        return current;
+        return userService.editCompany(id, new_company);
     }
 
     @PutMapping("/editPassword/{id}")
     public User editPassword(@PathVariable("id") Long id,
-                            @RequestBody String new_password) throws NoSuchElementException {
+                            @RequestBody String new_password)
+                            throws NoSuchElementException {
 
-//        Edits already established post by id.
-//        Try catch statement for the scenario where id is not found.
-//        Created current outside of try catch to ensure it was within scope for the return statement.
+        return userService.editPassword(id, new_password);
 
-        User current = null;
-        try {
-            current = userRepository.findById(id).get();
-            current.setPassword(new_password);
-            userRepository.save(current);
+    }
 
+    @PutMapping("/editDOB/{id}")
+    public User editDOB(@PathVariable("id") Long id,
+                             @RequestBody String new_DOB)
+            throws NoSuchElementException {
 
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-            System.out.println("No matching post could be found for id: " + id);
-        }
+        return userService.editDOB(id, new_DOB);
 
-        return current;
     }
 
 
-
-
-//
-//    @PutMapping("/user_like_a_post")
-//    public @ResponseBody String likePost(User user, Post post) {
-//
-////        Find the number of likes on a current post
-////        +1 the number of likes on post
-////        !Check |user methods| for liking a post method!
-//
-//        user.likePost(post);
-//
-////        Update the data to reflect likes +1
-//
-//        postService
-//
-////        Response
-//        return "Like added";
-//    }
 
 }
