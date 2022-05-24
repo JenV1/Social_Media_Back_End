c5e37589d3a5c79ff188be7b2dd1ab19988d8e3e
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-05-24 12:20:26 +0100

    Improving send message features

--- src/main/java/com/example/backEndProject/service/MessageService.java
+++ src/main/java/com/example/backEndProject/service/MessageService.java
@@ -3,6 +3,8 @@ package com.example.backEndProject.service;
 import com.example.backEndProject.model.Message;
 import com.example.backEndProject.model.User;
 import com.example.backEndProject.repository.MessageRepository;
+import com.example.backEndProject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +22,10 @@ public class MessageService {
         this.messageRepository = messageRepository;
     }
 
+    @Autowired
+    private UserRepository userRepository;
+
+
 
 //    END OF DEPENDENCY INJECTION
 //
@@ -35,7 +41,20 @@ public class MessageService {
         return messageRepository.save(message);
     }
 
-    public Message
+    public Message sendMessageToUser(String message_content,
+                                     String name_of_sender,
+                                     Long receiver_id) {
+
+        User receiver = userRepository.findByID(receiver_id);
+
+        Message newMessage = new Message(message_content, name_of_sender, receiver);
+
+        receiver.getInbox().add(newMessage);
+        newMessage.setUser(userRepository.findByID(receiver_id));
+
+
+        return messageRepository.save(newMessage);
+    }
 
 
 //    END OF METHODS
