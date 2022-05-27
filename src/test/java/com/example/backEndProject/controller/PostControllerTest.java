013a1e4216177822f79a9bedf291f6056d6f2d1f
scottiee-jc <s.christie496@gmail.com>
2022-05-27 10:30:03 +0100

    to main

--- src/test/java/com/example/backEndProject/controller/PostControllerTest.java
+++ src/test/java/com/example/backEndProject/controller/PostControllerTest.java
@@ -1,10 +1,17 @@
 package com.example.backEndProject.controller;
 
+import java.util.List;
+
+import com.example.backEndProject.model.Post;
+import com.example.backEndProject.model.dto.UserDto;
 import com.example.backEndProject.repository.PostRepository;
 import com.example.backEndProject.service.PostService;
+import org.apache.catalina.LifecycleState;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.print.DocFlavor;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,6 +20,9 @@ class PostControllerTest {
 
     @Autowired
     private PostController postController;
+
+    @Autowired
+    private PostService postService;
 
     @Test
     void getAll() {
@@ -59,10 +69,18 @@ class PostControllerTest {
 
     @Test
     void deletePostById() {
+        //        Given
+        String result = postController.deletePostByID(1L);
+        //        When
+
+        //        Then
+        assertEquals("Deleted Post 1. If this was a mistake, you can add a new post using the Add post function!", result);
     }
 
     @Test
-    void deletePostByID() {
+    void searchForAllBusinessAccountPosts() {
+        List result = postService.searchAllBusinessAccountPosts(true).stream().toList();
+        assertEquals(true, result);
     }
 
     @Test
